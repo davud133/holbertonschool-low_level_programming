b89f745c38cd3c612589622ca528e0050d4a4024
Davud <davudgeroqiq@gmail.com>
2025-12-03 12:50:22 +0000

    new commitment

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19")	
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
