8bd3c5009c67e257853d10f1add0eec62090c802
Davud <Davudgeroqiq@gmail.com>
2025-12-04 15:15:29 +0000

    new c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'a task'
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (j = 97; i < 102; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
