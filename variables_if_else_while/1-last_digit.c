ba93d752e3739193cc737e174f5d0634ab8b2776
Davud <Davudgeroqiq@gmail.com>
2025-12-04 14:19:57 +0000

    new c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'A program that prints "Hello, Betty!" to the console.'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
