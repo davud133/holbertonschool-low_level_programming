9225a9d1e6419aed7baa24089479d44dff97ce1a
Davud <Davudgeroqiq@gmail.com>
2025-12-04 15:02:04 +0000

    new c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description 'A program that prints numbers'
+ *
+ * Return: always 0 (succes)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
