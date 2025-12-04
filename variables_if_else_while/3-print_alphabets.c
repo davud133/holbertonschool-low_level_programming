9e3c734b818e0052f489c8aa5120bb67adc9ba0b
Davud <Davudgeroqiq@gmail.com>
2025-12-04 14:41:57 +0000

    new c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,11 +11,11 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 97; i < 122; i++)
+	for (i = 97; i < 123; i++)
 	{
 		putchar(i);
 	}
-	for (j = 65; j < 90; j++)
+	for (j = 65; j < 91; j++)
 	{
 		putchar(j);
 	}
