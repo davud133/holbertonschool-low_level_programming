03cbefbc7d0f500226cd269006091d399253cf62
Davud <Davudgeroqiq@gmail.com>
2025-12-04 15:05:45 +0000

    new c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Description: a task
+ *
+ * Return:0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 49; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
