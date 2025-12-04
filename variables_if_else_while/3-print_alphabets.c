937fe097715b97c04b98f804877c8e05cb00d097
Davud <Davudgeroqiq@gmail.com>
2025-12-04 14:37:29 +0000

    new c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'A program that prints "Hello, Betty!" to the console.'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 97; i < 122; i++)
+	{
+		putchar(i);
+	}
+	int i;
+
+	for(i = 65; i < 90; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n);
+	return (1);
+}
