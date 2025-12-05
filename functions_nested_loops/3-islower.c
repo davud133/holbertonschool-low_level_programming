6d8632e8b0ebf6b20f5481daa1c451b35d0e2db4
Davud <davudgeroqiq@gmail.com>
2025-12-05 13:41:09 +0000

    new commitment

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * _islower - checks if the char is lower characther
+ * _islower - checks if a character is lowercase
+ * @c: the character to check
  *
- * Return: int
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(char c)
 {
