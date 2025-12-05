634d54bafc5b24956d9c7f0c159dcb2937945a73
Davud <davudgeroqiq@gmail.com>
2025-12-05 14:10:42 +0000

    new commitment

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
  * _abs - gives absolute value
- * @n: a var that will be checked 
+ * @n: a var that will be checked
  * Return: absoulte value
  */
 int _abs(int n)
 {
 	if (n < 0)
 		return (-1 * n);
-	else
-		return (n);
+	return (n);
 }
