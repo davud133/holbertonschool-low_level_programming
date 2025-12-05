6f47fbad7cff343eea85cb84e471e1f0fc514c20
Davud <davudgeroqiq@gmail.com>
2025-12-05 13:58:47 +0000

    new commitment

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number which will be checked
+ *
+ * Return: a char and a number
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
+}
