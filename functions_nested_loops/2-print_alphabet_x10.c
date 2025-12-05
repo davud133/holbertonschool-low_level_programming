dad3eb4eaf18b55b7e05008517e29faae817c3a4
Davud <davudgeroqiq@gmail.com>
2025-12-05 13:32:47 +0000

    new commitment

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_10x - prints the alphabet 10 times
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 97; j < 123; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
