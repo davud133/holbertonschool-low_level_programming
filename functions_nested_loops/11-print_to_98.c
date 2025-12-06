c98eb4660620c5f55617e75499568f92cb74630b
Davud <davudgeroqiq@gmail.com>
2025-12-06 09:53:04 +0000

    new commitment

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,19 +8,16 @@
  */
 void print_to_98(int n)
 {
-	int i;
-	if (n == 98)
+	int i
+	if (n > 98)
 	{
-		printf("%d\n", 98);
-	}
-	else if (n > 98)
-	{
-		for (i = n; i > 97; i--)
+		for (i = n; i > 98; i--)
 			printf("%d, ", i);
 	}
 	else
 	{
-		for (i = n; i < 99; i++)
+		for (i = n; i < 98; i++)
 			printf("%d, ", i);
 	}
+	printf("%d\n",98);
 }
