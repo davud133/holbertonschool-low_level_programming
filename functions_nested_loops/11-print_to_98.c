8f667b1237df81428bbd01747853fe25874ebb2a
Davud <davudgeroqiq@gmail.com>
2025-12-06 09:35:55 +0000

    new commitment

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdiio.h>
+#include "main.h"
+/**
+ * print_to_98 - prints every number until 98
+ * @n: starts from this number
+ *
+ * Return: nothing
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; n > 97; n--)
+			printf("%d, ", i);
+	}
+	else
+	{
+		for (i = n; n < 99; n++)
+			printf("%d, ", i);
+	}
+}
