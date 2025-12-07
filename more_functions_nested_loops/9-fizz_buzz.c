0a9b7e58d6902540d624dfe48041b748567e972e
davud <davudgeroqiq@gmail.com>
2025-12-07 18:18:48 +0000

    new commit

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: run task
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i == 100)
+		{
+			printf("Buzz");
+		}	
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ",i)
+		}
+	}
+}
