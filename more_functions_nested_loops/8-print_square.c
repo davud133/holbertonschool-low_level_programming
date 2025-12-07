8f9d3a967972ea24cc2899b7360cc3d9e4083c0a
davud <davudgeroqiq@gmail.com>
2025-12-07 18:11:02 +0000

    new commit

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - it prints square by given size
+ * @size: size of the queare
+ *
+ * Return: nothing
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size == 0)
+		_putchar('\n');
+	else
+	{
+		for (i = size; i > 0; i--)
+		{
+			for (j = size; j > 0; j--)
+			{
+				_Putchar('#');
+			}
+			_putchar('#');
+		}
+	}
+}
