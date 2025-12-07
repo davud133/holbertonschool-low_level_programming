e4049b78f50309dfe2e2b32357e2f2e88c95a0d9
davud <davudgeroqiq@gmail.com>
2025-12-07 15:26:45 +0000

    new commit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - it multiples 2 numbers
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: an integer that is multiplyed  together
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
