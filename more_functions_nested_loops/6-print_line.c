00be44040efc691ded2cc8c6d27a714488da2ea0
davud <davudgeroqiq@gmail.com>
2025-12-07 15:42:51 +0000

    new commit

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - it prints n number of '-'
+ * @n: n is the count
+ *
+ * Return: nothing
+ */
+void print_line(int n)
+{
+	while ( n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
