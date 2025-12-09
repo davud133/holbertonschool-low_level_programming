fd9505b79979b760b724dc0ec3c9a58bffdf404f
Davud <davudgeroqiq@gmail.com>
2025-12-09 12:09:32 +0000

    new commitment

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - it reverses the string
+ * @str: the string
+ *
+ * Return: nothing
+ */
+void rev_string(char *str)
+{
+	int last_index = 0;
+	int i = 0;
+	char k;
+
+	while (str[last_index] != '\0')
+		last_index++;
+	last_index --;
+	for (i = 0; i < last_index / 2 + 1; i++)
+	{
+		char k = str[last_index];
+		str[last_index] = str[i];
+		str[i] = k;
+		last_index--;
+	}
+}	
