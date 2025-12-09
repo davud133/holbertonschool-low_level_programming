78634225cd1103f47549e679e3da7e06fa76e6a9
Davud <davudgeroqiq@gmail.com>
2025-12-09 12:15:55 +0000

    new commitment

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,11 +10,13 @@ void rev_string(char *str)
 	int last_index = 0;
 	int i = 0;
 	char k;
+	int b;
 
 	while (str[last_index] != '\0')
 		last_index++;
 	last_index--;
-	for (i = 0; i < last_index / 2; i++)
+	b = last_index;	
+	for (i = 0; i < b / 2; i++)
 	{
 		k = str[last_index];
 		str[last_index] = str[i];
