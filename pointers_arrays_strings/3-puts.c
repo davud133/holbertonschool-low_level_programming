f6cdf717156d0ce8f72ae38d1f1dda08d076f065
Davud <davudgeroqiq@gmail.com>
2025-12-09 11:51:45 +0000

    new commitment

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - it prints the string into stdout
+ * @str: the string
+ *
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+		
