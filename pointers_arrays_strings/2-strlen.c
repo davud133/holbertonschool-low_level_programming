97f6461eb67779a04a0f1a1fb430c98bffed6251
Davud <davudgeroqiq@gmail.com>
2025-12-09 11:47:11 +0000

    new commitment

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - it gives the size of string
+ * @c: the string
+ *
+ * Return: integer
+ */
+int _strlen(char *s)
+{
+	int size = 0;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		size++;
+		i++;
+	}
+	return (size);
+}
