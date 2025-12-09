a913472ac97417960b9b044f7cfeb07c40a847ee
Davud <davudgeroqiq@gmail.com>
2025-12-09 11:35:42 +0000

    new commitment

---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Task solutions about pointers
---
+++ pointers_arrays_strings/reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - it resets the n to 98
+ * @n: variable
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
