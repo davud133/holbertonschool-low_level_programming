6f19ff18bb84a5a33d89224afbcc7f8b11435e5f
Davud <Davudgeroqiq@gmail.com>
2025-12-10 12:33:55 +0000

    new commit

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcat - it appends one string to another
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: char pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	int last_index = 0;
+	int i;
+	int k = 0;
+	while dest[last_index] != '\0'
+		last_index++;
+	for (i = last_index; src[k] != '\0' ; i++)
+	{
+		dest[i] = src[k];
+		k++;
+	}
+	dest[last_index+k] = '\0';
+	return (dest);
+}
+
+
