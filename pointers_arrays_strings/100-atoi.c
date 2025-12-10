b43acfc50f0dd13edb94114a4f336c9f64a8e4c6
Davud <Davudgeroqiq@gmail.com>
2025-12-10 12:15:40 +0000

    new commit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,6 +13,7 @@ int _atoi(char *str)
 	int pow = 1;
 	int k;
 	int is_positive = 1;
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= '0' && str[i] <= '9')
@@ -31,17 +32,17 @@ int _atoi(char *str)
 		if (str[i] >= '0' && str[i] <= '9')
 		{
 			for (k = 0; k < int_count; k++)
-				pow*=10;
-			n+= (str[i] - '0') * pow;
+				pow *= 10;
+			n += (str[i] - '0') * pow;
 			int_count--;
 			pow = 1;
 		}
-		else 
+		else
 		{
 			if (int_count < 0)
 				break;
 		}
 	}
-	return (n*is_positive);
+	return (n * is_positive);
 
 }
