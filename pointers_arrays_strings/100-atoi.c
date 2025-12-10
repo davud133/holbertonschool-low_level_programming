a4ba0627835267abf38e3e2367455d44aed0a496
Davud <Davudgeroqiq@gmail.com>
2025-12-10 11:29:32 +0000

    new commit

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _atoi - it changes string to int
+ * @str: the string
+ *
+ * Return: integer
+ */
+int _atoi(char *str)
+{
+	int int_count = 0;
+	int i;
+	int n = 0;
+	int pow = 1;
+	int k;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+			int_count++;
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			for (k = 0; k < int_count; k++)
+				pow*=10;
+			n+= (str[i] - '0') * pow;
+			int_count--;
+			pow = 1;
+		}
+	}
+	return (n);
+
+}
