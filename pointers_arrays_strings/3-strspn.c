01457ec4b145b729bdf0637a76b96c1095172703
davud <davudgerqiq@gmail.com>
2025-12-14 18:00:47 +0000

    new commit

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strnspn - gets the length of a prefix substring
+ * @s: string
+ * @accept: accepted string
+ *
+ * Return: unsigned int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int k = 0;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (str[i] == accept[j])
+				k++;
+		}
+		if (k == 0)
+			break;
+	}
+	return (k);
+}
