43ee5049c2b849b64f2276410f98fc914f924bba
davud <davudgerqiq@gmail.com>
2025-12-14 15:42:35 +0000

    new commit

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * cap_string - capitalizez all words first characther
+ * @str: string
+ *
+ * Return: char
+ */
+char *cap_string(char *str)
+{
+	int i;
+	int k = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i] != ',' && str[i] != ';' && str[i] != '.' && str[i] != '!' && str[i] != '?' && str[i] != '"' && str[i] != '(' && str[i] != ')' && str[i] != '{' && str[i] != '}')
+		{
+			if ( k == 0)
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+		else
+		{
+			k = 0;
+		}
+	}
+	return (str);
+}
