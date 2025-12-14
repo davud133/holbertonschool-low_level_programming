893fe6c88ae0ef891d1ddb176d874727ecca0231
davud <davudgerqiq@gmail.com>
2025-12-14 15:16:56 +0000

    new commit

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - it reverses the arrya
+ * @a: array
+ * @n: number of elemts
+ *
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int k;
+
+	for (i = 0; i < n; i++)
+	{
+		k = a[n - i];
+		a[n - i] = a[i];
+		a[i] = k;
+	}
+}	
+
