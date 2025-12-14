f7258eeb7bbe052789a703d81c54c183b2429858
davud <davudgeroqiq@gmail.com>
2025-12-14 19:28:43 +0000

    new commit

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_chessboard - prints cheastboard
+ * @a: board
+ *
+ * Return: nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int k;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (k = 0; k < 8; k++)
+		{
+			_putchar(a[i][k]);
+		}
+	}
+}
