199419be5bf4ecdfc5a750e2822c034ae155578f
davud <davudgeroqiq@gmail.com>
2025-12-14 19:45:30 +0000

    new commit

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_diagsums - prints diagsums
  * @a: diagsum
@@ -9,12 +10,13 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int k;
-
-	for (i = 0; i < size; i++)
+	int sum1 = 0;
+	int sum2 = 0;
+	for (i = 0 ; i < size; i++)
 	{
-		for (k = 0; k < size; k++)
-			_putchar(a[i][k]);
-		_putchar('\n');
+		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + (size - i - 1));
 	}
+	printf("%d %d", sum1, sum2);
+
 }
