2a79665ca62a692d585336fb3c643e6fcf4061a2
davud <davudgerqiq@gmail.com>
2025-12-14 16:06:07 +0000

    new commit

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,19 +8,17 @@
 char *leet(char *str)
 {
 	int i;
+	char arr[10] = "aAeEoOtTlL";
+	char arr2[10] = "4433007711";
+	int k;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 'a' && str[i] == 'A')
-			str[i] = '4';
-		else if (str[i] == 'e' && str[i] == 'E')
-			str[i] = '3';
-		else if (str[i] == 'o' && str[i] == 'O')
-			str[i] = '0';
-		else if (str[i] == 't' && str[i] == 'T')
-			str[i] = '7';
-		else if (str[i] == 'l' && str[i] == 'L')
-			str[i] = '1';
+		for (k = 0; k < 10; k++)
+		{
+			if (str[i] == arr[k])
+				str[i] = arr2[k];
+		}
 	}
 	return (str);
 }
