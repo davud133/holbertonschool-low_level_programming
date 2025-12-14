06e4550f5711523dbfc52d890d398a4ce71dde89
davud <davudgerqiq@gmail.com>
2025-12-14 14:53:01 +0000

    new commit

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,7 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int lend = 0;
 	for (i = 0; src[i] != '\0'; i++)
 	       lens++;
-	for (i = 0; dest[i] = '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 		lend++;
 	for (i = 0; i < n; i++)
 	{
