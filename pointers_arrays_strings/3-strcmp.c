afbcbe882699a8365ca65901e01e56185013eb1a
davud <davudgerqiq@gmail.com>
2025-12-14 15:07:44 +0000

    new commit

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcmp - compares 2 string
+ * @s1: string 1
+ * @s2: sring 2
+ *
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2);
+{
+	int i = 0;
+
+	while (s1[i] == s2[i])
+	{
+		i++
+	}
+	return (s1[i] - s2[i]);
+}
