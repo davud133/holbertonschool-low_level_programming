ba54210a7ae175a73cfea6b2553904fd8d0b82f1
davud <davudgeroqiq@gmail.com>
2025-12-17 14:00:34 +0000

    new commit

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -7,11 +7,11 @@
  */
 void _print_rev_recursion(char *s)
 {
-	char *k = &s[0];
+	char *k = &s[1];
 	if (k[0] == '\0');
 	else
 	{
-		_print_rev_recursion(k[1]);
+		_print_rev_recursion(k);
 		_putchar(k[0]);
 	}
 }
