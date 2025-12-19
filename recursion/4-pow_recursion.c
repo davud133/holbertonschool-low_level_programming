d6591ee2e464b989551bd2d0c4f6531e6eff91c3
davud <davudgeroqiq@gmail.com>
2025-12-19 14:15:11 +0000

    new commit

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _pow_recursion - gives the x power of y
+ * @x: first number
+ * @y: second number
+ *
+ * Return: integer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (x);
+	else
+		return (x * _pow_recursion(x, y));
+}
+	
