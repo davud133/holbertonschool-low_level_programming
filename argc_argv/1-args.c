5647f9be96e1dab30de34a13abb9e26170be8773
davud <davudgeroqiq@gmail.com>
2025-12-21 05:46:32 -0400

    new commitment

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Description: prints the number of arguments
+ *
+ * Return: nothing
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
