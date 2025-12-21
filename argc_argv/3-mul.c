99007a6cc733e116c0eeaea2d0838e853fff0e28
davud <davudgeroqiq@gmail.com>
2025-12-21 05:58:46 -0400

    new commitment

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ * @argc: the number of arguments
+ * @argv: arguments
+ *
+ * Description: prints mul of 2 number
+ *
+ * Return: nothing
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 2)
+		printf("%d\n" atoi(argv[1]) * atoi(argv[2]));
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
