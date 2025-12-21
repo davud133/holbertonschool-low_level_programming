8c4c38c0285995dd5aeeb08629e71dc970553a6e
davud <davudgeroqiq@gmail.com>
2025-12-21 05:50:32 -0400

    new commitment

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Description: prints every arguments passed into
+ *
+ * Return: nothing
+ */
+int main(int argc, char *argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
