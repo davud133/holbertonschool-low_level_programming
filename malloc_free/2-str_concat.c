9b9946385aa8cec5a7d3f88ddfbb9c17d5c42e72
Davud <davudgeroqqiq@gmail.com>
2025-12-24 13:42:17 +0000

    new commit

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - concatenas 2 string
+ * @s1: string 1
+ * @s2: string 2
+ * Return: char pointer
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	int k;
+	int t;
+	char *cat;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+	for (k = 0; s2[k] != '\0'; k++)
+	{
+	}
+	cat = (char *)malloc((k + i + 1) * sizeof(char));
+	if (cat == NULL)
+		return (NULL);
+	for (t = 0; t < i; t++)
+		cat[t] = s1[t];
+	for (t = i; t < i + k + 1; t++)
+	       cat[t] = s2[t];
+	return (cat);
+}
