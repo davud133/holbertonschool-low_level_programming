af9062c1e867f5f24c861bab1eda00ba6a6b7e4a
Davud <davudgeroqqiq@gmail.com>
2025-12-24 13:18:14 +0000

    new commit

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -14,7 +14,7 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	for (k = 0; str[k] != '\0'; k++);	
+	for (k = 0; str[k] != '\0'; k++);
 	dup = (char *)malloc((k + 1) * sizeof(char));
 	if (dup == NULL)
 		return (NULL);
