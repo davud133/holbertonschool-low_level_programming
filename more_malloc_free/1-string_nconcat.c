eb3254aa417acbdaee334bc8d4d1c22883ec78be
davud <davudgeroqiq@gmail.com>
2025-12-28 15:48:02 +0000

    new commit

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,23 +14,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len_s2;
 	unsigned int i;
 	char *k;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (len_s1 = 0; s1[len_s1] != '\0' ; len_s1++)
-        {
-        }
+	for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
+	{
+	}
 	for (len_s2 = 0; s2[len_s2] != '\0' ; len_s2++)
-        {
-        }
+	{
+	}
 	if (n >= len_s2)
 	{
 		k = malloc(len_s1 + len_s2 + 1);
 		n = len_s2;
 	}
 	else
-		k = malloc(len_s1 + n + 1 );
+		k = malloc(len_s1 + n + 1);
 	if (k == NULL)
 		return (NULL);
 	for (i = 0; i < len_s1 + n; i++)
