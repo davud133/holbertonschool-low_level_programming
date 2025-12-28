17fb8911625d39b9d80fcf63b2dc5739cddf9092
davud <davudgeroqiq@gmail.com>
2025-12-28 17:29:52 +0000

    new commit

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 	array = malloc(sizeof(int) *( max - min + 1));
 	if (array == NULL)
 		return (NULL);
-	for (i = 0 ; i < (max - min); i++)
+	for (i = 0 ; i < (max - min + 1); i++)
 		array[i] = min + i;	
 	return (array);
 }
