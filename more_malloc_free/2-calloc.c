f9c1b8011885c4cd40d606433d6a9d69edcd4575
davud <davudgeroqiq@gmail.com>
2025-12-28 16:01:25 +0000

    new commit

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,8 +9,8 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *array;
-	int i;
+	char *array;
+	unsigned int i;
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	array = malloc(nmemb * size);
