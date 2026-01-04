cdf59029b1960cb8dc1a81d5315268f9a4c13930
davud <davudgeroqiq@gmail.com>
2026-01-04 17:29:22 +0000

    new commit

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+
+/* Add two numbers */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/* Subtract b from a */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/* Multiply two numbers */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/* Divide a by b */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/* Modulo of a by b */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
