a3956b923b58827114877c9ea54c647fec9ba8cb
davud <davudgeroqiq@gmail.com>
2026-01-04 17:03:11 +0000

    new commit

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,16 +11,16 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d.name == NULL)
+	if (d->name == NULL)
 		printf("Name: (nil)");
 	else
-		printf("Name: %s", d.name);
-	if (d.age == NULL)
+		printf("Name: %s", d->name);
+	if (d->age == NULL)
 		printf("(nil)");
 	else
-		printf("Age: %d", d.age);
-	if (d.owner == NULL)
+		printf("Age: %d", d->age);
+	if (d->owner == NULL)
 		printf("(nil)");
 	else
-		printf("Owner: %s", d.owner);
+		printf("Owner: %s", d->owner);
 }
