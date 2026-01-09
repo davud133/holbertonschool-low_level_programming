acd31d456b269c3954665dde73502e83ad112869
davud <davuderoqiq@gmail.com>
2026-01-09 12:44:17 +0000

    new commit

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(arg, n);
 	for (i = 0 ; i < n; i++)
 	{
-		if (i == 0)
+		if (i == 0 || separator == NULL)
 			printf("%d", va_arg(arg, int));
 		else
 			printf("%s%d", separator, va_arg(arg, int));
