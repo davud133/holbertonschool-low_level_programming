dcbfafb55e2905e1032489a9c324cb74bbff3063
davud <davudgeroqiq@gmail.com>
2026-01-10 20:46:51 +0300

    new commit

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,90 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_char
+ * @arg: char for printing
+ *
+ * Return: nothing
+ */
+void print_char(va_list arg)
+{
+	printf("%c", va_arg(arg, char));
+}
+
+/**
+ * print_int
+ * @arg: int for printing
+ *
+ * Return: nothing
+ */
+void print_int(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+
+/**
+ * print_str
+ * @arg: str for printing
+ *
+ * Return: nothing
+ */
+void print_str(va_list arg)
+{
+	char *s = va_arg(arg, char *);
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+}
+
+/**
+ * print_float
+ * @arg: float for printing
+ *
+ * Return: nothing
+ */
+void print_float(va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+
+/**
+ * print_all - print 4 types of arguments
+ * @format: type of arguments
+ * @...: arguments
+ *
+ * Return: Nothing
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	int k = 0;
+	va_list arg;
+	int outputed = 0;
+	char form[5] = {'c', 'i', 'f', 's', '\0'};
+	void (*f[4])(va_list);
+	f[0] = print_char;
+	f[1] = print_int;
+	f[2] = print_float;
+	f[3] = print_str;
+	va_start(arg, format);
+	while (format[i] != '\0')
+	{
+		while(form[k] != '\0')
+		{
+			if (format[i] == form[k])
+			{
+				if (outputed == 1)
+					printf(", ");
+				f[k](arg);
+				outputed = 1;
+			}
+			k++;
+		}
+		k = 0;
+		i++;
+	}
+	va_end(arg);
+	printf("\n");
+}
+		
