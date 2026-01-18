1bf18b29f3dc04ebb0f1beee5ad9e2aed30489ee
davud <davudgeroqiq@gmail.com>
2026-01-18 21:36:58 +0300

    new commit

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -19,15 +19,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 	new_node->n = n;
+
 	if (*h == NULL)
 	{
 		if (idx != 0)
 		{
-			free(new_node); 
+			free(new_node);
 			return (NULL);
 		}
 		new_node->prev = NULL;
@@ -35,35 +34,39 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 		return (new_node);
 	}
-    	if (idx == 0)
+
+	if (idx == 0)
 	{
 		new_node->next = *h;
-        	new_node->prev = NULL;
-        	(*h)->prev = new_node;
-        	*h = new_node;
-        	return (new_node);
-    	}
+		new_node->prev = NULL;
+		(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+
 	tmp = *h;
 	for (i = 0; i < idx - 1 && tmp->next != NULL; i++)
-        	tmp = tmp->next;
+		tmp = tmp->next;
+
 	if (tmp->next == NULL && i == idx - 1)
-    	{
-        	tmp->next = new_node;
-        	new_node->prev = tmp;
-        	new_node->next = NULL;
-    	}
-    	else if (i < idx - 1)
-    	{
-        	free(new_node);
-        	return (NULL);
-    	}
-    	else
-    	{
-        	new_node->next = tmp->next;
-        	new_node->prev = tmp;
-       		tmp->next->prev = new_node;
-        	tmp->next = new_node;
-    	}
-    return (new_node);
+	{
+		tmp->next = new_node;
+		new_node->prev = tmp;
+		new_node->next = NULL;
+	}
+	else if (i < idx - 1)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	else
+	{
+		new_node->next = tmp->next;
+		new_node->prev = tmp;
+		tmp->next->prev = new_node;
+		tmp->next = new_node;
+	}
+
+	return (new_node);
 }
 
