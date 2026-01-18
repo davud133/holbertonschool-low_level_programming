5fb1204dc4b19180fa7663d0d9369056f51527ea
davud <davudgeroqiq@gmail.com>
2026-01-18 21:41:06 +0300

    new commit

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,85 +1,69 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stddef.h>
 
-/**
- * insert_at_start - inserts a node at the beginning of a doubly linked list
- * @h: pointer to head
- * @new_node: new node to insert
- *
- * Return: address of new_node
- */
-dlistint_t *insert_at_start(dlistint_t **h, dlistint_t *new_node)
+/* Helper: insert at start of list */
+dlistint_t *insert_start(dlistint_t **h, int n)
 {
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *h;
-	(*h)->prev = new_node;
+
+	if (*h)
+		(*h)->prev = new_node;
+
 	*h = new_node;
+
 	return (new_node);
 }
 
-/**
- * insert_at_end - inserts a node at the end of a doubly linked list
- * @tmp: current last node
- * @new_node: node to insert
- *
- * Return: address of new_node
- */
-dlistint_t *insert_at_end(dlistint_t *tmp, dlistint_t *new_node)
+/* Helper: insert at end of list */
+dlistint_t *insert_end(dlistint_t *tail, int n)
 {
-	tmp->next = new_node;
-	new_node->prev = tmp;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = tail;
 	new_node->next = NULL;
+	tail->next = new_node;
+
 	return (new_node);
 }
 
-/**
- * insert_dnodeint_at_index - inserts node at a given index
- * @h: pointer to head of the list
- * @idx: index to insert at
- * @n: value of the new node
- *
- * Return: address of new node, or NULL if fail
- */
+/* Inserts a node at a given index in a doubly linked list */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
+	dlistint_t *tmp = *h;
 	dlistint_t *new_node;
-	dlistint_t *tmp;
+	unsigned int i;
+
+	if (!h)
+		return (NULL);
+
+	if (idx == 0 || !*h)
+		return (insert_start(h, n));
+
+	for (i = 0; i < idx - 1 && tmp->next; i++)
+		tmp = tmp->next;
+
+	if (!tmp->next && i == idx - 1)
+		return (insert_end(tmp, n));
+
+	if (i < idx - 1)
+		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
+
 	new_node->n = n;
-
-	if (*h == NULL)
-	{
-		if (idx != 0)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		new_node->prev = NULL;
-		new_node->next = NULL;
-		*h = new_node;
-		return (new_node);
-	}
-
-	if (idx == 0)
-		return (insert_at_start(h, new_node));
-
-	tmp = *h;
-	for (i = 0; i < idx - 1 && tmp->next != NULL; i++)
-		tmp = tmp->next;
-
-	if (tmp->next == NULL && i == idx - 1)
-		return (insert_at_end(tmp, new_node));
-	else if (i < idx - 1)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
 	new_node->next = tmp->next;
 	new_node->prev = tmp;
 	tmp->next->prev = new_node;
