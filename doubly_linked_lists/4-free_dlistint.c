93c8b4f67db4a4e5f527832d2e720eb6ad988942
davud <davudgeroqiq@gmail.com>
2026-01-18 19:14:51 +0300

    new commit

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * free_dlistint - frees doubly linked list
+ * @head: head node;
+ * 
+ * Return: nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (head != NULL)
+	{
+		if (head->next != NULL)
+		{
+			head = head->next;
+			free(head->prev);
+		}
+		free(head);
+	}
+}
+
