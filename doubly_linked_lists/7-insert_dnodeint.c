c00599e216c95444e0386d2471f80136861a84f1
davud <davudgeroqiq@gmail.com>
2026-01-18 21:31:40 +0300

    new commit

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -4,12 +4,12 @@
 #include <stdio.h>
 
 /**
- * insert_dnodeint_at_index - inserts a node at a given index
+ * insert_dnodeint_at_index - inserts node at a given index
  * @h: pointer to head of the list
  * @idx: index where to insert
  * @n: value of the new node
  *
- * Return: address of the new node, or NULL on failure
+ * Return: address of the new node, or NULL on failure / out-of-range
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
@@ -17,18 +17,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     dlistint_t *new_node;
     dlistint_t *tmp;
 
-    if (h == NULL)
-        return (NULL);
-
     new_node = malloc(sizeof(dlistint_t));
     if (new_node == NULL)
         return (NULL);
     new_node->n = n;
+
+    /* Empty list case */
     if (*h == NULL)
     {
         if (idx != 0)
         {
-            free(new_node);
+            free(new_node); /* cannot insert out-of-range */
             return (NULL);
         }
         new_node->prev = NULL;
@@ -36,6 +35,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
         *h = new_node;
         return (new_node);
     }
+
+    /* Insert at head */
     if (idx == 0)
     {
         new_node->next = *h;
@@ -44,21 +45,31 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
         *h = new_node;
         return (new_node);
     }
+
+    /* Walk the list to find insertion point */
     tmp = *h;
     for (i = 0; i < idx - 1 && tmp->next != NULL; i++)
         tmp = tmp->next;
+
+    /* Check if index is exactly at the end (tail) */
     if (tmp->next == NULL && i == idx - 1)
     {
         tmp->next = new_node;
         new_node->prev = tmp;
         new_node->next = NULL;
     }
+    else if (i < idx - 1)
+    {
+        /* Index is out-of-range */
+        free(new_node);
+        return (NULL);
+    }
     else
     {
+        /* Insert in the middle */
         new_node->next = tmp->next;
         new_node->prev = tmp;
-        if (tmp->next != NULL)
-            tmp->next->prev = new_node;
+        tmp->next->prev = new_node;
         tmp->next = new_node;
     }
 
