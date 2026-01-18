961e00753f2fe20992aafc3f2653823357fc9826
davud <davudgeroqiq@gmail.com>
2026-01-18 14:25:10 +0300

    new commit

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - prints list of nodes
+ * @h: Head node
+ * 
+ * Return: total number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("%s", h->str);
+		}
+		else
+		{
+			printf("[0] (nil)");
+		}
+		h = h->next;
+		n++;
+	}
+	return n;
+}
+
