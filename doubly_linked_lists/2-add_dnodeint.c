cc8c7c3fd55ad3e4d40a074ed7e85f5307ff7670
davud <davudgeroqiq@gmail.com>
2026-01-18 18:04:08 +0300

    new commit

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -17,8 +17,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	new_node->prev = *head;
-	new_node->next = (*head)->next;
-	(*head)->next = &new_node;
+	if (*head == NULL)
+	{
+    		new_node->prev = NULL;
+    		new_node->next = NULL;
+    		*head = new_node;
+    		return new_node;
+	}
+	new_node->prev = NULL;
+	new_node->next = *head;
+	(*head)->next = new_node;
 	return (new_node);
 }
