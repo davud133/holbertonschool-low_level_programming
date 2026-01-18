bcfdea841ff19d514b38df57c722440a375a65eb
davud <davudgeroqiq@gmail.com>
2026-01-18 22:02:26 +0300

    new commit

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -26,7 +26,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			return (1);
 		}
 		tmp->next->prev = NULL;
-		*head->next = tmp->next;
+		(*head)->next = tmp->next;
 		free(tmp);
 		return (1);
 	}
