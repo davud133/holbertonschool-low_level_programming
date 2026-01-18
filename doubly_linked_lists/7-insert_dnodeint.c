408a9dfb8625ae91a00312871c8a3c361bd3e66b
davud <davudgeroqiq@gmail.com>
2026-01-18 21:29:01 +0300

    new commit

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 /**
- * insert_dnodeint_at_index - inserts node at a given index
+ * insert_dnodeint_at_index - inserts a node at a given index
  * @h: pointer to head of the list
  * @idx: index where to insert
  * @n: value of the new node
@@ -16,6 +16,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     unsigned int i;
     dlistint_t *new_node;
     dlistint_t *tmp;
+
+    if (h == NULL)
+        return (NULL);
 
     new_node = malloc(sizeof(dlistint_t));
     if (new_node == NULL)
@@ -35,27 +38,27 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     }
     if (idx == 0)
     {
-	new_node->next = *h;
-    	new_node->prev = NULL;
-    	(*h)->prev = new_node;
-    	*h = new_node;
-    	return (new_node);
+        new_node->next = *h;
+        new_node->prev = NULL;
+        (*h)->prev = new_node;
+        *h = new_node;
+        return (new_node);
     }
     tmp = *h;
     for (i = 0; i < idx - 1 && tmp->next != NULL; i++)
         tmp = tmp->next;
-
     if (tmp->next == NULL && i == idx - 1)
     {
         tmp->next = new_node;
         new_node->prev = tmp;
         new_node->next = NULL;
     }
-    else 
+    else
     {
         new_node->next = tmp->next;
         new_node->prev = tmp;
-        tmp->next->prev = new_node;
+        if (tmp->next != NULL)
+            tmp->next->prev = new_node;
         tmp->next = new_node;
     }
 
