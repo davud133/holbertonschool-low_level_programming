7e7e062eb343ac220eeef24ffb3d8d080aceb2b2
davud <davudgeroqiq@gmail.com>
2026-01-18 14:59:16 +0300

    new commit

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,12 +12,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	new_node = malloc(size_of(list_t));
-	new_node->str = strdup(str);
 	int i;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return(NULL);
+	}
+	new_node->str = strdup(str);
 	for (i = 0 ; str[i] != '\0'; i++)
 	{}
 	new_node->len = i;
-	new_node->next = head;
+	new_node->next = *head;
 	*head = new_node;
+	return (new_node);
 }
