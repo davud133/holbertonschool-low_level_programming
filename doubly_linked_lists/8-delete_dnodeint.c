fac294d49e08282c7418badb0e702721cb84a811
davud <davudgeroqiq@gmail.com>
2026-01-18 21:50:13 +0300

    new commit

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * delete_dnodeint_at_index - deletes
+ * @head: head node;
+ * @index: index
+ *
+ * Return: nothing
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp;
+	unsigned int i;
+
+	if (*head == NULL)
+		return (-1);
+	tmp = *head;
+	for (i = 0 ; i < index; i++)
+	{
+		if (tmp->next != NULL)
+			tmp = tmp->next;
+		else
+			return (-1);
+	}
+	tmp->prev->next = tmp->next;
+	tmp->next->prev = tmp->prev;
+	free(tmp);
+	return (1);
+}
