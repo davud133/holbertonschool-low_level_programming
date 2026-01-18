27110d0ec83a761d868f9e6bdac26642187db45b
davud <davudgeroqiq@gmail.com>
2026-01-18 20:40:43 +0300

    new commit

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * sum_dlistint - sums every nodes'value
+ * @head: head node;
+ *
+ * Return: nothing
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head->next != NULL)
+	{
+		sum+= head->n;
+		head = head->next;
+	}
+	return (sum);
+}
