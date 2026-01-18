3dba60c5f8bbe0a9ea7db6bcfce9d44e6b4e62c8
davud <davudgeroqiq@gmail.com>
2026-01-18 22:08:37 +0300

    new commit

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -5,7 +5,3 @@ void before_main(void) __attribute__((constructor));
 void before_main(void) {
     printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
-
-int main() {
-    return 0;
-}
