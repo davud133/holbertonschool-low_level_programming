572dec52c1e134e30a47e782343f2b035d0f2143
davud <davudgeroqiq@gmail.com>
2026-01-18 15:32:26 +0300

    new commit

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * free_list - frees the list of nodes
+ * @head: start point
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	if (head != NULL)
+	{
+		list_t *next = head->next;
+		list_t *tmp;
+		while (next != NULL)
+		{
+			tmp = next;
+			next = tmp->next;
+			free(tmp);
+		}
+		free(head);
+	}
+}
+		
