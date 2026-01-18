579a38170a65949639f7a2bb20731655d0fb7d93
davud <davudgeroqiq@gmail.com>
2026-01-18 22:11:20 +0300

    new commit

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
-
+/**
+ * before_main - Prints a message before main() is executed
+ *
+ * This function demonstrates how to run code before the main function
+ * in C using the GCC/Clang constructor attribute. It will execute
+ * automatically before main() starts.
+ *
+ * Return: void
+ */
 void before_main(void) __attribute__((constructor));
 
-void before_main(void) 
+void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
