7c4bca9c60a192baa7e91d26b48606daf3c8d75b
davud <davudgeroqiq@gmail.com>
2026-01-18 21:35:58 +0300

    new commit

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -19,10 +19,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
+	{
 		return (NULL);
-    	new_node->n = n;
+	}
+	new_node->n = n;
 	if (*h == NULL)
-    	{
+	{
 		if (idx != 0)
 		{
 			free(new_node); 
