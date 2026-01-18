9cc0518e21094a0c4ec900c0aa903e60c7469966
davud <davudgeroqiq@gmail.com>
2026-01-18 21:19:57 +0300

    new commit

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -28,10 +28,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	tmp = *h;
 	if (idx == 0)
 	{
-		tmp->prev = new_node;
 		new_node->prev = NULL;
-		new_node->next = tmp;
+		new_node->next = NULL;
 		*h = new_node;
+		return (new_node);
 	}
 	else
 	{
