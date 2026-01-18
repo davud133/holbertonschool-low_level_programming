6f9d407a893f5a70268da187ccb9f501f23efae2
davud <davudgeroqiq@gmail.com>
2026-01-18 17:51:15 +0300

    new commit

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * dlistint_len - print list of doubly linked list
+ * @h: head;
+ *
+ * Return: len of list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	dlistint *tmp;
+	size_t len = 0;
+
+	if (h == NULL)
+		return (0);
+	if (h->prev != NULL)
+	{
+		tmp = h->prev;
+		while (tmp->prev != NULL)
+		{
+			tmp = tmp->prev;
+		}
+	}
+	else
+	{
+		if (h->next != NULL)
+		{
+			tmp = h->next->prev;
+		}
+		else
+		{
+			return (1);
+		}
+		while (tmp != NULL)
+		{
+			len++;
+			tmp = tmp->next;
+		}
+	}
+	return (len);
+}
