796190d6ba5a55a3be98da2c30c7cc3b5e6f909e
davud <davudgeroqiq@gmail.com>
2026-01-20 02:03:22 +0300

    new commit

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all combo of 2 digits
+ *
+ * Return: nothing;
+ */
+int main(void)
+{
+	int a = 0;
+	int b = 1;
+	while (a != 9)
+	{
+		while (b != 10)
+		{
+			if (a != 0 || b != 1)
+				putchar(' ');
+			putchar(a + '0');
+			putchar(b + '0');
+			if (a != 8 || b != 9)
+				putchar(',');
+			b++;
+		}
+		a++;
+		b = a + 1;
+	}
+	return (0);
+}
