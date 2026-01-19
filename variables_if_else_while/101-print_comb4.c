53218b67b7b94ad8cb3490055523aa1a2e27afff
davud <davudgeroqiq@gmail.com>
2026-01-20 02:20:01 +0300

    new commit

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -16,14 +16,13 @@ int main(void)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
-                		putchar(k + '0');
-                		if (!(i == 7 && j == 8 && k == 9))
-                		{
-                    			putchar(',');
-                    			putchar(' ');
-                		}
-            		}
-    	putchar('\n');
-    	return 0;
+				putchar(k + '0');
+				if (!(i == 7 && j == 8 && k == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+	putchar('\n');
+	return 0;
 }
-
