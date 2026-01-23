306a9e1725cae7ea18ac27433006c2d23fe1890d
davud <davudgeroqiq@gmail.com>
2026-01-23 16:14:28 +0300

    new commit

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,14 +22,14 @@ int create_file(const char *filename, char *text_content)
 	if (k == -1)
 	{
 		free(text_content);
-		return (0);
+		return (-1);
 	}
 	n = write(k, text_content, length);
 	if (n == -1)
 	{
 		free(text_content);
 		close(k);
-		return (0);
+		return (-1);
 	}
 	close(k);
 	return (1);
