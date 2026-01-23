71c206ff5e9f509062232578661ee4413a2ffbd0
davud <davudgeroqiq@gmail.com>
2026-01-23 16:12:51 +0300

    new commit

---
+++ file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * create_file - creates file
+ * @filename: name of the file
+ * @text_content: content of the file
+ *
+ * Return: 1 on success 0 on fail
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int k;
+	int n;
+	int length;
+	if (text_content == NULL)
+		text_content = "";
+	for (length = 0; text_content[length] != '\0'; length++)
+	{
+	}
+	if (filename == NULL)
+		return (-1);
+	k = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (k == -1)
+	{
+		free(text_content);
+		return (0);
+	}
+	n = write(k, text_content, length);
+	if (n == -1)
+	{
+		free(text_content);
+		close(k);
+		return (0);
+	}
+	close(k);
+	return (1);
+}
