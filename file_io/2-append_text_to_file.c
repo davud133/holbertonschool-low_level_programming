5d99bd2fdbea2b2bb5ed083319e35a3c1ae0e34d
davud <davudgeroqiq@gmail.com>
2026-01-23 16:27:49 +0300

    new commit

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text toe nd of a file
+ * @filename: name of file
+ * @text_content: text
+ *
+ * Return: 1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int length;
+	int k;
+
+	if (filename == NULL)
+		return (-1);
+	for (length = 0; text_content[length] != '\0'; length++)
+	{
+	}
+	k = open(filename, O_WONLY);
+	if (k == -1)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+		return (1);
+	w = write(filename, text_content, length);
+	if (w == -1)
+		return (-1);
+	return (1);
+}
+
