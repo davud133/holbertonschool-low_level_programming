9141aaf9b20e8ddf21af8cf2472d33abdb2386c3
davud <davudgeroqiq@gmail.com>
2026-01-23 16:34:07 +0300

    new commit

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	ssize_t w;
 
 	if (filename == NULL)
-		return (-1);	
+		return (-1);
 	k = open(filename, O_WRONLY | O_APPEND);
 	if (k == -1)
 	{
