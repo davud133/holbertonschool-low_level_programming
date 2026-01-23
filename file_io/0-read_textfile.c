d80c43f8e60afca05455f3ca7510374d85442031
davud <davudgeroqiq@gmail.com>
2026-01-23 15:33:15 +0300

    new commit

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,16 +9,19 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t n;
+	int k;
+	char *str;
+	int r;
+	int w;
 	if (filename == NULL)
 		return (0);
-	int k = open(filename,O_RDONLY);
+	k = open(filename,O_RDONLY);
 	if (k == -1)
 		return (0);
-	char *str;
-	int r = read(k,str,letters);
+	r = read(k,str,letters);
 	if (r != letters)
 		return (0);
-	int w = write(k,str,r);
+	w = write(k,str,r);
 	if (w != r)
 		return (0);
 	return (r);
