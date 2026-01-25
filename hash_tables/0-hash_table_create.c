19388684688f2d139b1acd6eacfa35b02e38b019
davud <davudgeroqiq@gmail.com>
2026-01-25 21:41:18 +0300

    new commit

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table;
+	unsigned long int i;
 
 	new_hash_table = malloc(sizeof(hash_table_t));
 	if (new_hash_table == NULL)
@@ -20,5 +21,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(new_hash_table);
 		return (NULL);
 	}
+	for (i = 0; i < size; i++)
+		new_hash_table->array[i] = NULL;
 	return (new_hash_table);
 }
