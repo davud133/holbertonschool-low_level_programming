6a874eb8ccf42ed18addd5a02d9596e52270cd2e
davud <davudgeroqiq@gmail.com>
2026-01-25 20:45:07 +0300

    new commit

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -34,8 +34,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		else
 		{
-		new_node->next = ht->array[index];
-		ht->array[index] = new_node;
+			new_node->next = ht->array[index];
+			ht->array[index] = new_node;
 		}
 	}
 	return (1);
