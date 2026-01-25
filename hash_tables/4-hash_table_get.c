d82955b2ccdde13b02e631bf5ea6efe298a936d4
davud <davudgeroqiq@gmail.com>
2026-01-25 21:09:22 +0300

    new commit

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *tmp;
 
-	if (key == NULL)
+	if (ht == NULL)
 		return ("(null)");
 	tmp = ht->array[hash_djb2((unsigned char *)key) % ht->size];
 	while (tmp != NULL)
