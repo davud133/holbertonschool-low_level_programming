4163615bb45f717678e4b108e8f9a84929da1c49
davud <davudgeroqiq@gmail.com>
2026-01-25 20:15:55 +0300

    new commit

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -11,7 +11,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	if (key == NULL)
 		return (-1);
-	if (size == 0) 
+	if (size == 0)
 		return (-1);
 	return (hash_djb2(key) % size);
 }
