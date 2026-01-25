d9b6690826ca05e32be0f469dfd0d949be79c492
davud <davudgeroqiq@gmail.com>
2026-01-25 17:17:30 +0300

    new commit

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -18,63 +18,63 @@ int main(int argc, char *argv[])
 		return (-1);
 	if (argc < 3 || argc > 3)
 	{
-		write(2,"Usage: cp file_from file_to\n",28);
+		write(2, "Usage: cp file_from file_to\n", 28);
 		return (97);
 	}
 	fdt = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	fdf = open(argv[1], O_RDONLY);
 	if (fdf == -1)
 	{
-		write(STDERR_FILENO,"Error: Can't read from file ",28);
+		write(STDERR_FILENO, "Error: Can't read from file ", 28);
 		for (i = 0; argv[1][i] != '\0'; i++)
 		{
-			write(STDERR_FILENO, &argv[1][i],1);
+			write(STDERR_FILENO, &argv[1][i], 1);
 		}
-		write(STDERR_FILENO,"\n",1);
+		write(STDERR_FILENO, "\n", 1);
 		return (98);
 	}
 	if (fdt == -1)
 	{
-		write(2,"Error: Can't write to ",22);
+		write(2, "Error: Can't write to ", 22);
 		for (i = 0; argv[2][i] != '\0'; i++)
 		{
-			write(2, &argv[2][i],1);
+			write(2, &argv[2][i], 1);
 		}
-		write(2,"\n",1);
+		write(2, "\n", 1);
 		return (99);
 	}
 	do
 	{
-	r = read(fdf,str,1024);
+	r = read(fdf, str, 1024);
 	if (r == -1)
 	{
-		write(2,"Error: Can't read from file ",28);
+		write(2, "Error: Can't read from file ", 28);
 		for (i = 0; argv[1][i] != '\0'; i++)
 		{
-			write(2, &argv[1][i],1);
+			write(2, &argv[1][i], 1);
 		}
-		write(2,"\n",1);
+		write(2, "\n", 1);
 		return (98);
 	}
 	if (r == 0)
 		break;
 	while (written < r)
 	{
-		w = write(fdt,str + written ,r - written);
+		w = write(fdt, str + written, r - written);
 		if (w == -1)
 		{
-			write(2,"Error: Can't write to ",22);
+			write(2, "Error: Can't write to ", 22);
 			for (i = 0; argv[2][i] != '\0'; i++)
 			{
-				write(2, &argv[2][i],1);
+				write(2, &argv[2][i], 1);
 			}
-			write(2,"\n",1);
+			write(2, "\n", 1);
 			return (99);
 		}
 		written += w;
 	}
 	written = 0;
-	}while (1);
+	} while (1);
 	if (close(fdf) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fdf);
