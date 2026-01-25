b4d2c70ba62d4f7b581aaac597e77f0b3905e13e
davud <davudgeroqiq@gmail.com>
2026-01-25 21:37:28 +0300

    new commit

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * hash_table_delete - deletes hash table
+ * @ht: hash table
+ *
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp;
+	hash_node_t *tmp2;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+
+		while (tmp != NULL)
+		{
+			tmp2 = tmp->next;
+			free(tmp->value);
+			free(tmp->key);
+			free(tmp);
+			tmp = tmp2;
+		}
+	}
+	free(ht->array);
+}
