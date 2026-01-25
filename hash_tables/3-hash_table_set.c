de74c3d5e575b45d21de15065fe3d887a0c20ff5
davud <davudgeroqiq@gmail.com>
2026-01-25 20:52:56 +0300

    new commit

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -33,7 +33,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		tmp = ht->array[index];
 		while (tmp != NULL)
 		{
-			if (strcmp(tmp->key,key) == 0)
+			if (strcmp(tmp->key, key) == 0)
 			{
 				free(tmp->value);
 				tmp->value = strdup(value);
