8d1903c2c164b66de60dee6fdd88caa32b7bd3dc
davud <davudgeroqiq@gmail.com>
2026-01-25 21:03:22 +0300

    new commit

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get- return the value asssosicated with the key
+ * @ht: table
+ * @key: key
+ *
+ * Return: value or NULL if not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *tmp;
+
+	tmp = ht->array[hash_djb2((unsigned *char)key) % ht->size];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			return (tmp->value);
+		}
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
