d91b2206c052efe5536e55ae508f98a4d0bd93f4
davud <davudgeroqiq@gmail.com>
2026-01-25 19:31:23 +0300

    new commit

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * hash_table_create - creates a new hash table
+ * @size: size of the hash table
+ *
+ * Return: pointer to newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t new_hash_table;
+
+	new_hash_table = malloc(sizeof(hash_table_t));
+	if (new_hash_table == NULL)
+		return (NULL);
+	new_hash_table->size = size;
+	new_hash_table->array = malloc(sizeof(hash_node_t) * 1024);
+	if (new_hash_table->array == NULL)
+		return (NULL);
+	return (new_hash_table);
+}
