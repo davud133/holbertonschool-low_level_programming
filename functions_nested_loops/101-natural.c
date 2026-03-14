a9f55a4d13282df8a6c77d8f46b88f0be7ae572e
davud <davudgeroqiq@gmail.com>
2026-03-15 00:41:22 +0300

    new commit

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -12,11 +12,11 @@ int main()
 	int sum = 0;
 	int i;
 
-	for (i = 1; i < n; i++)
+	for (i = 1; i < n;i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 			sum += i;
 	}
-	printf("%d", sum);
+	printf("%d\n", sum);
 	return (0);
 }
