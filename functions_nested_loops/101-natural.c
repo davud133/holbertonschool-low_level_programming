1b6fb79f310d31ccd6ee9f2d9fa21e46ba3b6b4c
davud <davudgeroqiq@gmail.com>
2026-03-15 00:38:45 +0300

    new commit

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * 
+ * Description: prints the sum of all the multiples of 3 or 5 below 1024
+ *
+ * Return: 1 on success 0 on failure
+ */
+int main()
+{
+	int n = 1024;
+	int sum = 0;
+	itn i;
+
+	for (int i = 1, i < n; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	printf("%d", sum);
+}
