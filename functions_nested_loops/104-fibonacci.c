d3b07c7ef3467f40d08642494005798fb4808536
davud <davudgeroqiq@gmail.com>
2026-03-15 01:19:05 +0300

    new commit

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,39 +1,46 @@
 #include <stdio.h>
 
+/**
+ * main - Entry Point
+ *
+ * Description: 98th fib num
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    unsigned long a_low = 1, b_low = 2;
-    unsigned long a_high = 0, b_high = 0;
-    unsigned long next_low, next_high;
-    unsigned long base = 1000000000;
-    int i;
+	unsigned long a_low = 1, b_low = 2;
+	unsigned long a_high = 0, b_high = 0;
+	unsigned long next_low, next_high;
+	unsigned long base = 1000000000;
+	int i;
 
-    printf("1, 2");
+	printf("1, 2");
 
-    for (i = 3; i <= 98; i++)
-    {
-        next_low = a_low + b_low;
-        next_high = a_high + b_high;
+	for (i = 3; i <= 98; i++)
+	{
+		next_low = a_low + b_low;
+		next_high = a_high + b_high;
 
-        if (next_low >= base)
-        {
-            next_low -= base;
-            next_high += 1;
-        }
+		if (next_low >= base)
+		{
+			next_low -= base;
+			next_high += 1;
+		}
 
-        if (next_high > 0)
-            printf(", %lu%09lu", next_high, next_low);
-        else
-            printf(", %lu", next_low);
+		if (next_high > 0)
+			printf(", %lu%09lu", next_high, next_low);
+		else
+			printf(", %lu", next_low);
 
-        a_low = b_low;
-        a_high = b_high;
-        b_low = next_low;
-        b_high = next_high;
-    }
+		a_low = b_low;
+		a_high = b_high;
+		b_low = next_low;
+		b_high = next_high;
+	}
 
-    printf("\n");
+	printf("\n");
 
-    return 0;
+	return (0);
 }
 
