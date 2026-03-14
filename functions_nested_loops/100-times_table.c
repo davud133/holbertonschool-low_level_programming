a1f2072f5d3172af05776e24e3b478addee2ba8d
davud <davudgeroqiq@gmail.com>
2026-03-15 00:02:50 +0300

    new commit

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,6 +1,24 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
+/**
+ * Helper - prints the number
+ * @n: int n
+ *
+ * Return: nothing
+ */
+void  Helper(int n)
+{
+	int i;
+	int num[4] = {-1, -1, -1, -2};
+
+	for (i = 0; n > 0; i++)
+	{
+		num[i] = n % 10;
+		n = n / 10;
+	}
+	for (i = 0; num[i] != -2; i++)
+		write(1, num[i] + '0', 1);
+}
+
 /**
  * print_times_table - prints 'n' times table
  * @n: dimension of table
@@ -18,9 +36,11 @@ void print_times_table(int n)
 			for (k = 0; k < n + 1; k++)
 			{
 				if (k != n)
-					printf("%d, ", i * k);
+					Helper(i * k);
+					write(1, ",", 1);
+					write(1, " ", 1);
 				else
-					printf("%d\n", i * k);
+					Helper(i * k);
 			}
 		}
 	}
