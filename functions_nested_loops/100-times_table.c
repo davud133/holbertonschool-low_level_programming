5c1670f743971d1e45766dd25410ded2b919f350
davud <davudgeroqiq@gmail.com>
2026-03-15 00:07:41 +0300

    new commit

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -9,14 +9,17 @@ void  Helper(int n)
 {
 	int i;
 	int num[4] = {-1, -1, -1, -2};
-
+	int l;
 	for (i = 0; n > 0; i++)
 	{
 		num[i] = n % 10;
 		n = n / 10;
 	}
 	for (i = 0; num[i] != -2; i++)
-		write(1, num[i] + '0', 1);
+	{
+		l = num[i];
+		write(1, l + '0', 1);
+	}
 }
 
 /**
@@ -36,9 +39,11 @@ void print_times_table(int n)
 			for (k = 0; k < n + 1; k++)
 			{
 				if (k != n)
+				{
 					Helper(i * k);
-					write(1, ",", 1);
-					write(1, " ", 1);
+					write(1, ',', 1);
+					write(1, ' ', 1);
+				}
 				else
 					Helper(i * k);
 			}
