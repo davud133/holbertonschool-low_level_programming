8bea2d108945855e629cb8b861e2e70ed2351065
davud <davudgeroqiq@gmail.com>
2026-03-15 23:29:21 +0300

    new commit

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * print_number - prints number
+ * @n: number
+ *
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	if (n < 0)
+		_putchar('-');
+	while (n > 0)
+	{
+		_putchar(n % 10 + '0');
+		n = n / 10;
+	}
+}
